5c3c56542c5a8161d9a32e62647511bd2835984c
Sonu Mondal <takeinfo2u@gmail.com>
2023-07-31 12:53:20 +0530

    Updation in total electronics project

--- Total electronics project/script.js
+++ Total electronics project/script.js
@@ -8,6 +8,17 @@ function showbar(){
         x.innerText=arr[i]
         menubar.appendChild(x)
         x.style.listStyle="none"
+        if(i==0){
+            x.style.paddingTop="2rem"
+            x.style.paddingBottom="5px"
+        }
+        else if(i==3){
+            x.style.paddingTop="5px"
+            x.style.paddingBottom="2rem"
+        }
+        else{
+            x.style.padding="5px 0"
+        }
     }
     count=1
    }
