84edf1b7fae3956edb7000f6342fef9610edde44
Sonu Mondal <takeinfo2u@gmail.com>
2023-08-23 15:48:35 +0530

    Created: To do web app >>  tic-tac-toe-game-web-app >> Update: sonumondal(My Portfolio) Temp-converter-web-app

--- Temp Converter/script.js
+++ Temp Converter/script.js
@@ -1,7 +1,36 @@
-let input=document.getElementsByTagName("input")
-let span=document.getElementsByTagName("span")
-function getAns(){
-    let c=input[0].value
-    let f=(9*c)/5+32
-    span[0].innerText=f;
+let converToCelcius = false;
+let converToFahrenheit = false;
+let inputTag = document.getElementById("inputTag");
+let ans=document.getElementById("ans")
+function cToF() {
+  inputTag.placeholder = "Enter temperature in degree celcius";
+  converToFahrenheit = true;
+  converToCelcius=false
+  inputTag.value=""
+  ans.style.display="none"
+}
+function fToC() {
+  inputTag.placeholder = "Enter temperature in degree fahrenheit";
+  converToCelcius = true;
+  converToFahrenheit=false
+  inputTag.value=""
+  ans.style.display="none"
+}
+function getAnsInCelcius() {
+    if (converToCelcius == true) {
+        console.log("To celcius");
+        let f = inputTag.value;
+        let c = 5*(f-32)/9;
+        ans.innerText=`Celcius: ${c}`
+        ans.style.display="block"
+      }
+}
+function getAnsInFahrenheit() {
+  if (converToFahrenheit == true) {
+    console.log("To fh");
+    let c = inputTag.value;
+    let f = (9 * c) / 5 + 32;
+    ans.innerText=`Fahrenheit: ${f}`
+    ans.style.display="block"
+  }
 }
